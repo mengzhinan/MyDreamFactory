0b72eb1f07e10b51cd036b820ad44386b77ab7e6
杜科 <duke@zhihu.com>
2019-11-13 17:07:04 +0800

    init

--- app/src/main/java/com/duke/mydreamfactory/MainActivity.kt
+++ app/src/main/java/com/duke/mydreamfactory/MainActivity.kt
@@ -12,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        startActivity(intentFor<TabActivity>())
+//        startActivity(intentFor<TabActivity>())
 
     }
 }
